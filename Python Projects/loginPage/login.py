049c6991b1b7bada4e050b2a29046f400a99a64c
EchoGhost84 <nathanbboudreau@icloud.com>
2023-12-19 15:47:45 -0800

    Login with Password Gen
    trying to add the login page with a password genorator and have it output both login lists and password lists to files

--- Python Projects/loginPage/login.py
+++ Python Projects/loginPage/login.py
@@ -3,24 +3,26 @@ Userlist = ["Nathan","nathan"]
 adminPassword = ["ADMINPASSWORD"]
 Passwordlist = ["Password1","password1"]
 
+while():
+    if input() in Userlist:
+        print("User Correct")
+    else:
+        print("User Incorrect")
+
+    if input() in Passwordlist:
+            print("Password Correct")
+    else:
+            print("Password Incorrect")
+    break
+
 while input == adminPassword:
         print("What would you like to add to the UserList?")
         Userlist.append(input)
+        print("Updated List", Userlist)
+        continue
+else:
+    while input == adminPassword:
         print("What would you like to add to the Passwordlist?")
         Passwordlist.append(input)
-        
-else:
-    while():
-        if input() in Userlist:
-            print("User Correct")
-        else:
-            print("User Incorrect")
-
-        if input() in Passwordlist:
-            print("Password Correct")
-        else:
-            print("Password Incorrect")
-        break
-
-
-
+        print("Updated List", Passwordlist)
+        continue
