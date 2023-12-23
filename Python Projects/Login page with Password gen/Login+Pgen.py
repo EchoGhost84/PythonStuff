e4ac2ef1d501aef14df2a57b87cb4bb1ed7408bd
EchoGhost84 <nathanbboudreau@icloud.com>
2023-12-23 14:44:12 -0800

    updated

--- Python Projects/Login page with Password gen/Login+Pgen.py
+++ Python Projects/Login page with Password gen/Login+Pgen.py
@@ -1,35 +1,35 @@
 #note this may not work and its in a test phase
 import random
-
+import os
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
     'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
     'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
     'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-Userlist = ["Nathan","nathan"]
+Userlist = open(r"C:\Users\Natha\OneDrive\Desktop\PythonStuff\Python Projects\Login page with Password gen\UserList.txt",'r')
+Passwordlist = open(r"C:\Users\Natha\OneDrive\Desktop\PythonStuff\Python Projects\Login page with Password gen\PasswordList.txt",'r')
+adminUsername = ["ADMINUSER"]
 adminPassword = ["ADMINPASSWORD"]
-Passwordlist = ["Password1","password1"]
-
 #makes sure the username and password are correct
 def user():
     print("Welcome to the Login Page")
-    print(input("Please enter your username\n"))
+    print("Please enter your username")
 
-    while input() in Userlist:
+    if input() in Userlist:
         print("User Correct")
-    if input not in Userlist:
+    else:
         print("User Incorrect")
 
-    print(input("Pease enter your password\n"))
-    while input() in Passwordlist:
-        print("User Correct")
-    if input not in Passwordlist:
-        print("User Incorrect")
+    print("Pease enter your password")
+    if input() in Passwordlist:
+        print("Password Correct")
+    else:
+        print("Password Incorrect")
 
 #removes a password from the list of passwords if needed to 
 def remove():
-    if input == adminPassword:
+    if input in user() == adminPassword:
         rempass = input("Enter the password you want to remove")
         while rempass:
             print("what password would you like to remove?")
@@ -38,7 +38,7 @@ def remove():
 
 #just a password genorator that saves to a file
 def gen():
-    while input == adminPassword:
+    if input in user() == adminPassword:
             print("Welcome to the PyPassword Generator!")
             nr_letters = int(input("How many letters would you like in your password?\n"))
             nr_symbols = int(input("How many symbols would you like?\n"))
