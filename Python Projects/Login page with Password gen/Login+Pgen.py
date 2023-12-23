59795f9f724933cb207cb92ab53b769fad4267fd
EchoGhost84 <nathanbboudreau@icloud.com>
2023-12-23 15:52:13 -0800

    file locations
    added file locations for password lists and user lists as well as admin stuff

--- Python Projects/Login page with Password gen/Login+Pgen.py
+++ Python Projects/Login page with Password gen/Login+Pgen.py
@@ -1,35 +1,35 @@
 #note this may not work and its in a test phase
 import random
-import os
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
     'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
     'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
     'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-Userlist = open(r"C:\Users\Natha\OneDrive\Desktop\PythonStuff\Python Projects\Login page with Password gen\UserList.txt",'r')
-Passwordlist = open(r"C:\Users\Natha\OneDrive\Desktop\PythonStuff\Python Projects\Login page with Password gen\PasswordList.txt",'r')
-adminUsername = ["ADMINUSER"]
-adminPassword = ["ADMINPASSWORD"]
+Userlist = open(r"C:\Users\Natha\OneDrive\Desktop\PythonStuff\Python Projects\Login page with Password gen\UserList.txt",'r').readlines()
+Passwordlist = open(r"C:\Users\Natha\OneDrive\Desktop\PythonStuff\Python Projects\Login page with Password gen\PasswordList.txt",'r').readlines()
+adminUsername =  open(r"C:\Users\Natha\OneDrive\Desktop\PythonStuff\Python Projects\Login page with Password gen\AdminUser.txt",'r').readlines()
+adminPassword =  open(r"C:\Users\Natha\OneDrive\Desktop\PythonStuff\Python Projects\Login page with Password gen\AdminPassword.txt",'r').readlines()
 #makes sure the username and password are correct
 def user():
     print("Welcome to the Login Page")
     print("Please enter your username")
 
-    if input() in Userlist:
+    username = input().strip().lower()
+    if username in Userlist or adminUsername:
         print("User Correct")
     else:
         print("User Incorrect")
 
     print("Pease enter your password")
-    if input() in Passwordlist:
+    password =input().strip()
+    if password in Passwordlist or adminPassword:
         print("Password Correct")
     else:
         print("Password Incorrect")
-
+    
 #removes a password from the list of passwords if needed to 
 def remove():
-    if input in user() == adminPassword:
         rempass = input("Enter the password you want to remove")
         while rempass:
             print("what password would you like to remove?")
@@ -38,8 +38,7 @@ def remove():
 
 #just a password genorator that saves to a file
 def gen():
-    if input in user() == adminPassword:
-            print("Welcome to the PyPassword Generator!")
+            print("Welcome to the Password Generator!")
             nr_letters = int(input("How many letters would you like in your password?\n"))
             nr_symbols = int(input("How many symbols would you like?\n"))
             nr_numbers = int(input("How many numbers would you like?\n"))
@@ -62,7 +61,7 @@ def gen():
                 password += char
 
             print(input("Would you like to add this password to the list?"))
-            while input in list == ["yes", "Yes", "YES"]:
+            while input == ["yes", "Yes", "YES"]:
                 password = ''.join(password_list) + '\n'
                 Passwordlist.append(password)
             print("Updated List", Passwordlist)
@@ -70,7 +69,7 @@ def gen():
                 file.write(Passwordlist)
             print(f": Updated list: {Passwordlist}\n") 
             while(): 
-                input in list == ["no", "No", "NO", "nO"]
+                input == ["no", "No", "NO", "nO"]
                 break
 
 
