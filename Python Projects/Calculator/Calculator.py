c8747e6b1994853c9979a1939ba42cd81fe11daa
EchoGhost84 <nathanbboudreau@icloud.com>
2024-01-07 09:32:22 -0800

    Update Calculator.py

--- Python Projects/Calculator/Calculator.py
+++ Python Projects/Calculator/Calculator.py
@@ -1,69 +1,54 @@
-# just makes all the math funtions into their own class
-def addition(x, y):
-    return x + y
+# Define math functions as a class
+class MathOperations:
+    @staticmethod
+    def addition(x, y):
+        return x + y
 
-def subtraction(x, y):
-    return x - y
+    @staticmethod
+    def subtraction(x, y):
+        return x - y
 
-def multiplication(x, y):
-    return x * y
+    @staticmethod
+    def multiplication(x, y):
+        return x * y
 
-def division(x, y):
-    return x / y
+    @staticmethod
+    def division(x, y):
+        return x / y
 
-# makes sure you pick the right thing and tells you to get the first and second number
-# and adds, subtracts, multiplys, or divides them 
+# Create an instance of the MathOperations class
+math_operations = MathOperations()
+
+# Main loop for user interaction
 while True:
+    choice = input("1. Addition  2. Multiplication  3. Subtraction  4. Division\n")
     
-    choice = input("1. Addition" + ' ' + "2. Multiplication" + ' ' + "3. Subtraction" + ' '  "4. Division\n")
-    if choice in ("1. Addition", "2. Multiplication", "3. Subtraction", "4. Division"):
+    # Check if the user's choice is valid
+    if choice in ("1", "2", "3", "4"):
         try:
-            number1 = float(input("gimmie first number: \n"))
-            number2 = float(input("gimmie second number: \n"))
+            number1 = float(input("Enter the first number: \n"))
+            number2 = float(input("Enter the second number: \n"))
         except ValueError:
-            print("Please enter a number.")
+            print("Please enter valid numbers.")
             continue
 
-# makes sure your doing the right equation and prints it out
-        if choice == "1. Addition":
-            print(number1, "+", number2, "=", addition(number1, number2))
+        # Perform the chosen math operation and print the result
+        if choice == "1":
+            print(number1, "+", number2, "=", math_operations.addition(number1, number2))
+        elif choice == "2":
+            print(number1, "x", number2, "=", math_operations.multiplication(number1, number2))
+        elif choice == "3":
+            print(number1, "-", number2, "=", math_operations.subtraction(number1, number2))
+        elif choice == "4":
+            print(number1, "/", number2, "=", math_operations.division(number1, number2))
 
-        if choice == "2. Multiplication":
-            print(number1, "x", number2, "=", multiplication(number1, number2))
+        # Open 'Math.txt' file and append the result
+        with open('Math.txt', 'a') as file:
+            file.write(f"{number1} {choice} {number2} = {math_operations.__getattribute__('addition' if choice == '1' else 'multiplication' if choice == '2' else 'subtraction' if choice == '3' else 'division')(number1, number2)}\n")
 
-        if choice == "3. Subtraction":
-            print(number1, "-", number2, "=", subtraction(number1, number2))
-
-        if choice == "4. Division": 
-            print(number1, "/", number2, "=", division(number1, number2))
-            
-        while():
-            pwd = ''.join(addition) + '\n'
-            with open('Math.txt', 'a') as file:
-                    file.write(pwd)
-            print(f"Your random password is: {pwd}\n")
-
-            pwd = ''.join(multiplication) + '\n'
-            with open('Math.txt', 'a') as file:
-                    file.write(pwd)
-            print(f"Your random password is: {pwd}\n")
-
-            pwd = ''.join(subtraction) + '\n'
-            with open('Math.txt', 'a') as file:
-                    file.write(pwd)
-            print(f"Your random password is: {pwd}\n")
-
-            pwd = ''.join(division) + '\n'
-            with open('Math.txt', 'a') as file:
-                    file.write(pwd)
-            print(f"Your random password is: {pwd}\n")
-
-# asks if you want to do another equation
-        calculation = input("another? (yes/no): \n")
-        if calculation == "no":
-                break
-        else:
-                print("Invalid Input\n")
-
-
-
+        # Ask if the user wants to perform another calculation
+        calculation = input("Do another calculation? (yes/no): \n")
+        if calculation.lower() == "no":
+            break
+    else:
+        print("Invalid choice. Please enter 1, 2, 3, or 4.")
